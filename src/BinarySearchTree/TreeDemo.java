06018248fc3e327528c1222208b623e4fc0d0c19
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-07-05 22:05:04 +0300

    BST Insert Method.

--- src/BinarySearchTree/TreeDemo.java
+++ src/BinarySearchTree/TreeDemo.java
@@ -4,4 +4,12 @@ package BinarySearchTree;
  * Created by Robert-PC on 7/5/2017.
  */
 public class TreeDemo {
+
+    public TreeDemo(){
+
+    }
+
+    public void getDemo(){
+
+    }
 }
--- src/BinarySearchTree/TreeManager.java
+++ src/BinarySearchTree/TreeManager.java
@@ -1,7 +1,40 @@
 package BinarySearchTree;
 
+import sun.reflect.generics.tree.Tree;
+
+import java.util.Optional;
+
 /**
  * Created by Robert-PC on 7/5/2017.
  */
 public class TreeManager {
+
+    TreeNode root;
+
+    public TreeManager(){
+
+    }
+
+    public TreeManager(TreeNode root){
+        this.root = root;
+    }
+
+
+    public TreeNode insert(TreeNode treeNode, int data){
+        if(treeNode == null){
+            // inserting new node
+            return new TreeNode(data);
+        }
+
+        if(data <= treeNode.getData()){
+            //searching in left side of the tree
+            treeNode.setLeft(insert(treeNode.getLeft(),data));
+        }
+        else{
+            treeNode.setRight(insert(treeNode.getRight(),data));
+        }
+
+        return treeNode;
+
+    }
 }
