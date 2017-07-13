e44ba44e7462f99de5b63d1e3c3405e94bcb853f
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-07-13 15:24:33 +0300

    AVL balanced Tree template.

---
+++ src/BinarySearchTree/TreeTransformer.java
@@ -0,0 +1,22 @@
+package BinarySearchTree;
+
+/**
+ * Created by Robert-PC on 7/13/2017.
+ */
+public class TreeTransformer {
+
+    public TreeTransformer(TreeNode root){
+    }
+
+    public TreeNode transformToAVL(TreeNode root){
+
+    }
+
+    private TreeNode leftRotation(TreeNode node){
+
+    }
+
+    private TreeNode rightRotation(TreeNode node){
+
+    }
+}
