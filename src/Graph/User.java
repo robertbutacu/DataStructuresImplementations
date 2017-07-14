a43a54e34e69fd11ed1ed0fbf0f92a29a6563b4b
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-07-14 23:05:55 +0300

    Nuttin' much.

--- src/Graph/User.java
+++ src/Graph/User.java
@@ -11,6 +11,20 @@ import java.util.Optional;
  */
 public class User {
 
+
+    /*
+        IMPORTANT!!!
+
+            Having implemented Graph data structure AFTER implementing BST, I didn't know i was gonna implement the Graph as a
+        network of users. Saying that, the ordering in the BST doesn't make much sense, as nodes are stored by their data
+        (which doesn't mean much in the context of a user), and not by their username(as it would make much more sense).
+
+            You are free to make any necessary changes.
+
+            TODO change ordering in BST to be by username, and not by data.
+
+     */
+
     //current user
     TreeNode user;
 
--- src/Graph/UserDemo.java
+++ src/Graph/UserDemo.java
@@ -4,4 +4,13 @@ package Graph;
  * Created by Robert-PC on 7/14/2017.
  */
 public class UserDemo {
+
+    public UserDemo(){
+
+    }
+
+    public void getDemo(){
+
+
+    }
 }
